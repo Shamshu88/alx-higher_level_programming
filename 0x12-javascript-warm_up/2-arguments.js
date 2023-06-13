f54f8ed60f01dd3bac9f154f91722815ddd6fbfe
Shamshu88 <rogersemaduk@gmail.com>
2023-06-13 00:30:43 +0000

    0x12. JavaScript - Warm up

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.argv.lenght == 2) {
+console.log('No argument');
+} else if (procesee.argv.lenght == 3) {
+console.log('Argument found');
+} else {
+console.log('Argument found');
+}
