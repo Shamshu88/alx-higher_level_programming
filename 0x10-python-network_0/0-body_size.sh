af31d4a42dbf911f0cc4c99c067413da05494fd9
Shamshu88 <rogersemaduk@gmail.com>
2023-06-30 16:22:39 -0700

    0x10-python-network_0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL
-curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
+# Get the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c'
