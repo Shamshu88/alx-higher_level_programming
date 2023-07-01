220419c9a44e029f0c32526ccb2c729315fd732e
Shamshu88 <rogersemaduk@gmail.com>
2023-06-30 17:37:54 -0700

    0x10-python-network_0

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#pass a variable in the header of the request
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+# takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+curl -sX GET $1 -H "X-HolbertonSchool-User-Id: 98" -L
