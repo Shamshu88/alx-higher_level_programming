bd2eec0dfbd4fe07bc19705fdb5c928eeb311c99
Shamshu88 <rogersemaduk@gmail.com>
2023-07-05 15:13:12 -0700

    0x10. Python - Network #0

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -sX GET $1 -H "X-HolbertonSchool-User-Id: 98" -L
+# send a GET request to the URL using curl, and display the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
